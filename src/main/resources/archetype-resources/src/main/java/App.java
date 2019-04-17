631ab35489519b2f0448c7bf8578d3ad5894b0c9
AK <akhan789@hotmail.com>
2019-04-17 17:06:37 +0100

    First commit.

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.solidwater.disgaea5bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
