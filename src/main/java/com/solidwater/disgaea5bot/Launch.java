64c9976e93b2567a7510bca04c40d838ae23e5d5
akhan789 <akhan789@hotmail.com>
2019-04-25 16:31:17 +0100

    Added base address and the basics of a dynamic address.

--- src/main/java/com/solidwater/disgaea5bot/Launch.java
+++ src/main/java/com/solidwater/disgaea5bot/Launch.java
@@ -21,8 +21,12 @@ public class Launch /* extends Application */ {
 	public static void main(String[] args) {
 		try {
 			WindowsUtils.openProcess("Disgaea 5 Complete");
-			//System.out.println(WindowsUtils.getFloatValue(0x07A6A518l, 0x00000008l, 8l));
-			System.out.println(WindowsUtils.getIntValue(0x19323A7C620l, 0, 4l));
+			//System.out.println(WindowsUtils.getFloatValue(0x07A6A518l, new long[] {0x00000008l}, 4l));
+			//System.out.println(WindowsUtils.getFloatValue(0x07A6A518l, new long[] { 0x00000008l }, 16l));
+			//System.out.println(WindowsUtils.getIntValue(0x1D1DCB67EC0l, new long[] {0}, 4l));
+			System.out.println(WindowsUtils.getIntValue(0x00B66A20l, new long[] {0x98l, 0x4D8l}, 4l));
+			//System.out.println(Long.toHexString(Long.parseLong("3664289952")));
+			//System.out.println(WindowsUtils.getIntValue(0x00B66A20l, new long[] {0x0l}, 4l));
 			// WindowsUtils.openProcess("disgaea5.exe");
 		} catch (WindowsAPIException e) {
 			e.printStackTrace();
--- src/main/java/com/solidwater/disgaea5bot/util/WindowsUtils.java
+++ src/main/java/com/solidwater/disgaea5bot/util/WindowsUtils.java
@@ -12,7 +12,10 @@ import com.sun.jna.Memory;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.Kernel32Util;
+import com.sun.jna.platform.win32.Psapi.MODULEINFO;
 import com.sun.jna.platform.win32.WinDef.DWORD;
+import com.sun.jna.platform.win32.WinDef.HMODULE;
 import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.platform.win32.WinNT;
 import com.sun.jna.platform.win32.WinNT.HANDLE;
@@ -30,6 +33,7 @@ public class WindowsUtils {
 	static final ExtendedPsapi PSAPI;
 
 	static volatile HANDLE process = null;
+	static volatile long processBaseAddress = 0l;
 
 	static {
 		USER_32 = ExtendedUser32.INSTANCE;
@@ -43,14 +47,14 @@ public class WindowsUtils {
 				WinNT.TOKEN_QUERY | WinNT.TOKEN_ADJUST_PRIVILEGES, hToken);
 		if (!success) {
 			throw new RuntimeException("OpenProcessToken failed. Error " + Native.getLastError() + ": "
-					+ WindowsAPIException.getWindowsAPIErrorMessage(Native.getLastError()));
+					+ Kernel32Util.getLastErrorMessage());
 		}
 
 		WinNT.LUID luid = new WinNT.LUID();
 		success = ADVAPI_32.LookupPrivilegeValue(null, WinNT.SE_DEBUG_NAME, luid);
 		if (!success) {
 			throw new RuntimeException("LookupPrivilegeValue failed. Error " + Native.getLastError() + ": "
-					+ WindowsAPIException.getWindowsAPIErrorMessage(Native.getLastError()));
+					+ Kernel32Util.getLastErrorMessage());
 		}
 
 		WinNT.TOKEN_PRIVILEGES tkp = new WinNT.TOKEN_PRIVILEGES(1);
@@ -58,7 +62,7 @@ public class WindowsUtils {
 		success = ADVAPI_32.AdjustTokenPrivileges(hToken.getValue(), false, tkp, 0, null, null);
 		if (!success) {
 			throw new RuntimeException("AdjustTokenPrivileges failed. Error " + Native.getLastError() + ": "
-					+ WindowsAPIException.getWindowsAPIErrorMessage(Native.getLastError()));
+					+ Kernel32Util.getLastErrorMessage());
 		}
 		Kernel32.INSTANCE.CloseHandle(hToken.getValue());
 	}
@@ -103,51 +107,71 @@ public class WindowsUtils {
 				System.out.println(windowTitle + " process opened successfully");
 			} else {
 				throw new WindowsAPIException("OpenProcess failed. Error " + Native.getLastError() + ": "
-						+ WindowsAPIException.getWindowsAPIErrorMessage(Native.getLastError()));
+						+ Kernel32Util.getLastErrorMessage());
 			}
 		} else {
 			throw new WindowsAPIException("GetWindowThreadProcessId failed. Error " + Native.getLastError() + ": "
-					+ WindowsAPIException.getWindowsAPIErrorMessage(Native.getLastError()));
+					+ Kernel32Util.getLastErrorMessage());
 		}
 	}
 
-	public static float getFloatValue(long address, long offset, long bytesToRead) throws WindowsAPIException {
-		if(WindowsUtils.process == null)
+	public static float getFloatValue(long baseAddress, long[] offsets, long bytesToRead) throws WindowsAPIException {
+		if (WindowsUtils.process == null)
 			throw new WindowsAPIException("Process not initialised correctly");
 
 		IntByReference bytesRead = new IntByReference(0);
+		long processBaseAddress = WindowsUtils.getBaseAddressForProcess(1024);
+		System.out.println("Process base address: " + Long.toHexString(processBaseAddress));
+		baseAddress += processBaseAddress;
+		long address = baseAddress;
+		for (long offset : offsets) {
+			address += offset;
+		}
+		System.out.println("Scanning address: " + Long.toHexString(address) + " from base address: "
+				+ Long.toHexString(baseAddress));
 		Memory buffer = new Memory(bytesToRead);
 		if (WindowsUtils.KERNEL_32.ReadProcessMemory(WindowsUtils.process, new Pointer(address), buffer,
 				(int) buffer.size(), bytesRead)) {
 			System.out.println("Bytes read: " + bytesRead.getValue());
-			return buffer.getFloat(offset);
+			return buffer.getFloat(0l);
 		} else {
 			throw new WindowsAPIException("ReadProcessMemory failed. Error " + Native.getLastError() + ": "
-					+ WindowsAPIException.getWindowsAPIErrorMessage(Native.getLastError()));
+					+ Kernel32Util.getLastErrorMessage());
 		}
 	}
 
-	public static void setFloatValue(long address, float newValue) {
+	public static void setFloatValue(long baseAddress, float newValue) {
 
 	}
 
-	public static int getIntValue(long address, long offset, long bytesToRead) throws WindowsAPIException {
-		if(WindowsUtils.process == null)
+	public static int getIntValue(long baseAddress, long[] offsets, long bytesToRead) throws WindowsAPIException {
+		if (WindowsUtils.process == null)
 			throw new WindowsAPIException("Process not initialised correctly");
 
 		IntByReference bytesRead = new IntByReference(0);
+		long processBaseAddress = WindowsUtils.getBaseAddressForProcess(1024);
+		System.out.println("Process base address: " + Long.toHexString(processBaseAddress));
+		System.out.println("Value base address: " + Long.toHexString(baseAddress));
+		long dynamicAddress = WindowsUtils.getDynamicAddress(processBaseAddress, baseAddress, bytesToRead);
+		System.out.println("Dynamic address: " + Long.toHexString(dynamicAddress));
+		long address = dynamicAddress;
+		for (long offset : offsets) {
+			address += offset;
+		}
+		System.out.println("Scanning address: " + Long.toHexString(address) + " from base address: "
+				+ Long.toHexString(baseAddress));
 		Memory buffer = new Memory(bytesToRead);
 		if (WindowsUtils.KERNEL_32.ReadProcessMemory(WindowsUtils.process, new Pointer(address), buffer,
 				(int) buffer.size(), bytesRead)) {
 			System.out.println("Bytes read: " + bytesRead.getValue());
-			return buffer.getInt(offset);
+			return buffer.getInt(0l);
 		} else {
 			throw new WindowsAPIException("ReadProcessMemory failed. Error " + Native.getLastError() + ": "
-					+ WindowsAPIException.getWindowsAPIErrorMessage(Native.getLastError()));
+					+ Kernel32Util.getLastErrorMessage());
 		}
 	}
 
-	public static void setIntValue(long address, float newValue) {
+	public static void setIntValue(long baseAddress, float newValue) {
 
 	}
 
@@ -155,7 +179,7 @@ public class WindowsUtils {
 		if (WindowsUtils.process != null) {
 			if (!WindowsUtils.KERNEL_32.CloseHandle(WindowsUtils.process)) {
 				throw new WindowsAPIException("CloseHandle failed. Error " + Native.getLastError() + ": "
-						+ WindowsAPIException.getWindowsAPIErrorMessage(Native.getLastError()));
+						+ Kernel32Util.getLastErrorMessage());
 			} else {
 				System.out.println("Process closed successfully");
 			}
@@ -166,5 +190,57 @@ public class WindowsUtils {
 
 	public static boolean checkRemoteDebuggerPresent() throws WindowsAPIException {
 		return false;
+	}
+
+	protected static long getDynamicAddress(long processBaseAddress, long baseAddress, long bytesToRead)
+			throws WindowsAPIException {
+		IntByReference bytesRead = new IntByReference(0);
+		Memory buffer = new Memory(bytesToRead);
+		processBaseAddress += baseAddress;
+		if (WindowsUtils.KERNEL_32.ReadProcessMemory(WindowsUtils.process, new Pointer(processBaseAddress), buffer,
+				(int) buffer.size(), bytesRead)) {
+			System.out.println("Bytes read: " + bytesRead.getValue());
+			return buffer.getInt(0l);
+		} else {
+			throw new WindowsAPIException("ReadProcessMemory failed. Error " + Native.getLastError() + ": "
+					+ Kernel32Util.getLastErrorMessage());
+		}
+	}
+
+	protected static long getBaseAddressForProcess(int modulesLength) throws WindowsAPIException {
+		HMODULE[] lphModules = new HMODULE[modulesLength];
+		IntByReference lpcbNeeded = new IntByReference(0);
+		if (WindowsUtils.PSAPI.EnumProcessModules(WindowsUtils.process, lphModules, lphModules.length, lpcbNeeded)) {
+			if (lphModules.length < lpcbNeeded.getValue()) {
+				return WindowsUtils.getBaseAddressForProcess(lpcbNeeded.getValue());
+			} else {
+				byte[] moduleFileName = new byte[1024];
+				for (HMODULE module : lphModules) {
+					if (WindowsUtils.PSAPI.GetModuleFileNameExA(WindowsUtils.process, module, moduleFileName,
+							moduleFileName.length) != 0) {
+						String fileName = Native.toString(moduleFileName);
+						System.out.println("Module found: " + fileName);
+						if (fileName.contains("disgaea5.exe")) {
+							System.out.println("Found disgaea5.exe module");
+							MODULEINFO moduleInfo = new MODULEINFO();
+							if (WindowsUtils.PSAPI.GetModuleInformation(WindowsUtils.process, module, moduleInfo,
+									moduleInfo.size())) {
+								return Long.valueOf(String.valueOf(Pointer.nativeValue(moduleInfo.lpBaseOfDll)));
+							} else {
+								throw new WindowsAPIException("GetModuleInformation failed. Error "
+										+ Native.getLastError() + ": " + Kernel32Util.getLastErrorMessage());
+							}
+						}
+					} else {
+						throw new WindowsAPIException("GetModuleFileNameExA failed. Error " + Native.getLastError()
+								+ ": " + Kernel32Util.getLastErrorMessage());
+					}
+				}
+			}
+		} else {
+			throw new WindowsAPIException("EnumProcessModules failed. Error " + Native.getLastError() + ": "
+					+ Kernel32Util.getLastErrorMessage());
+		}
+		return 0l;
 	}
 }
